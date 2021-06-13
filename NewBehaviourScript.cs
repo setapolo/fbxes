0536a39fd4550b9b631d3cc196ddcb640a398aa0
m.oka <okamino@gmail.com>
2021-06-13 15:44:57 +0900

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+   // Start is called before the first frame update
+   void Start()
+   {
+Debug.Log ("<color=#ff0000ff>debug comment</color>"); // 赤色で表示
+Debug.Log ("<color=#00ff00ff>debug comment</color>"); // 緑色で表示
+Debug.Log ("<color=#0000ffff>debug comment</color>"); // 青色で表示
+Debug.Log ("<i>debug comment</i>");             //斜体で表示
+Debug.Log ("<b>debug comment</b>");             //太文字で表示
+Debug.Log ("<size=30>debug comment</size>");    //大きく表示        
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+       
+   }
+}
